74c1a31d5db2037f06a8770a1e87b3d8bff7972f
eugeMolinero <euge_molinero@yahoo.com.ar>
2016-02-05 11:52:27 -0300

    Estilos sección productos

--- html/productos/6anos.php
+++ html/productos/6anos.php
@@ -1,15 +1,22 @@
-<h3 class="t02">6 años<span>®</span></h3>
-<p>6 años.</p>
+<h3 class="t6">Etapa de transición</h3>
+<p>Limpieza y protección contra las caries en la fase de transición para los dientes adultos.</p>
 <div class="prodImg">
     <img src="../images/productos/colgate-6.png" alt="Mi primer Colgate, para niños menores de 2 años">
 </div>
-<div class="globoProd gl02">
-    <h2>Cepillo dental Colgate®</h2>
-    <p class="edad">+6 años</p>
-    <ul class="ul02">
-        <li>Para que los padres ayuden a los niños a cepillarse los dientes.</li>
-        <li>Cerdas extra suaves para una limpieza suave y efectiva.</li>
-        <li>Cabeza compacta para la boca pequeña del niño.</li>
-        <li>Mango anti-deslizante que se adapta a la mano del niño y padre para el cepillado.</li>
+<div class="globoProd gl6a">
+    <h2>Enjuague bucal Colgate® Plax Kids - Magic</h2>
+    <p class="edadSm">6+ años</p>
+    <h2 style="margin-bottom:5px">Bob Esponja</h2>
+    <p>Bob Esponja tenía muy mal aliento y no sabía como resolver esta situación. Nadie se le podía acercar ya que si abría la boca todo el mundo empezaba a correr. Así que su odontólogo le recomendó usar después de cepillarse los dientes enjuague bucal Colgate Plax.</p>
+</div>
+
+<div class="globoProd gl6b">
+    <h2>Crema dental Colgate® Smiles </h2>
+    <p class="edadSm">6+ años</p>
+    <h2 style="margin-bottom:5px">Barbie - Spiderman - Bob Esponja</h2>
+    <ul class="ul6">
+        <li>Protección contra las caries.</li>
+        <li>Delicioso sabor tutti-frutti.</li>
+        <li>Cantidad adecuada de fluoruro para los niños.</li>
     </ul>
 </div>
