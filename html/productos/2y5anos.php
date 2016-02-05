94ddc0ab8875008f38c01da578987f9a16abc94c
eugeMolinero <euge_molinero@yahoo.com.ar>
2016-02-05 11:06:58 -0300

    archivos imgs y productos

--- html/productos/2y5anos.php
+++ html/productos/2y5anos.php
@@ -1,15 +1,27 @@
-<h3 class="t02">2 y 5 años<span>®</span></h3>
-<p>2 y 5 años.</p>
+<h3 class="t25">Protección de los dientes de leche</h3>
+<p>Cremas dentales con flúor y cepillos de cerdas suaves que proporcionan limpieza y protección eficaces. Con los personajes preferidos de los chicos, para ayudar a hacer la rutina siempre divertida.</p>
 <div class="prodImg">
     <img src="../images/productos/colgate-25.png" alt="Mi primer Colgate, para niños menores de 2 años">
 </div>
-<div class="globoProd gl02">
-    <h2>Cepillo dental Colgate®</h2>
-    <p class="edad">2 - 5 años</p>
-    <ul class="ul02">
-        <li>Para que los padres ayuden a los niños a cepillarse los dientes.</li>
-        <li>Cerdas extra suaves para una limpieza suave y efectiva.</li>
-        <li>Cabeza compacta para la boca pequeña del niño.</li>
-        <li>Mango anti-deslizante que se adapta a la mano del niño y padre para el cepillado.</li>
+<div class="globoProd gl25a">
+    <h2>Crema dental Colgate® Smiles</h2>
+    <p class="edadSm">2 - 5 años</p>
+    <h2 style="margin-bottom:5px">Barbie - Bob Esponja</h2>
+    <ul class="ul25">
+        <li>Protección contra las caries.</li>
+        <li>Delicioso sabor tutti-frutti.</li>
+        <li>Cantidad adecuada de fluoruro para los niños.</li>
+    </ul>
+</div>
+
+<div class="globoProd gl25b">
+    <h2>Cepillo dental Colgate® Smiles</h2>
+    <p class="edadSm">2 - 5 años</p>
+    <h2 style="margin-bottom:5px">Barbie - Bob Esponja</h2>
+    <ul class="ul25">
+        <li>Cerdas extra suaves en varios niveles que alcanzan todos los dientes, incluyendo las muelas difíciles de alcanzar.</li>
+        <li>Cerdas coloridas en el centro que indican la cantidad recomendada de crema dental.</li>
+        <li>Cabeza pequeña y con forma ovalada, con suaves materiales que protegen las encías de los niños.</li>
+        <li>Formato anatómico.</li>
     </ul>
 </div>
--- html/productos/5anos.php
+++ html/productos/5anos.php
@@ -1,15 +1,28 @@
-<h3 class="t02">5 años<span>®</span></h3>
-<p>5 años.</p>
+<h3 class="t5">Etapa de transición</h3>
+<p>Limpieza y protección contra las caries en la fase de transición para los dientes adultos.</p>
 <div class="prodImg">
     <img src="../images/productos/colgate-5.png" alt="Mi primer Colgate, para niños menores de 2 años">
 </div>
-<div class="globoProd gl02">
-    <h2>Cepillo dental Colgate®</h2>
+<div class="globoProd gl5a">
+    <h2>Cepillo dental Colgate®<br>
+	Princess - Sports</h2>
     <p class="edad">+5 años</p>
-    <ul class="ul02">
-        <li>Para que los padres ayuden a los niños a cepillarse los dientes.</li>
-        <li>Cerdas extra suaves para una limpieza suave y efectiva.</li>
-        <li>Cabeza compacta para la boca pequeña del niño.</li>
-        <li>Mango anti-deslizante que se adapta a la mano del niño y padre para el cepillado.</li>
+    <ul class="ul5">
+        <li>Cabeza ovalada pequeña que protege las encías.</li>
+        <li>Cerdas extra suaves que proporcionan una limpieza real de varios niveles de los dientes, grandes o pequeños.</li>
+        <li>Formato anatómico.</li>
+    </ul>
+</div>
+
+<div class="globoProd gl5b">
+    <h2>Cepillo dental Colgate® Smiles</h2>
+    <p class="edadSm">+5 años</p>
+    <h2 style="margin-bottom:5px">Barbie - Spiderman</h2>
+    <ul class="ul5">
+        <li>Limpiador de lengua en la parte posterior de la cabeza del cepillo de un material suave, proporciona una limpieza completa de la boca.</li>
+        <li>Cerdas coloridas en el centro que indican la cantidad recomendada de crema dental.</li>
+        <li>Cabeza ovalada pequeña que protege las encías.</li>
+        <li>Cerdas extra suaves que proporcionan una limpieza real de varios niveles de los dientes, grandes o pequeños.</li>
+        <li>Formato anatómico.</li>
     </ul>
 </div>
