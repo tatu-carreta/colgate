b7b8bfa1c7a46c9bd326ed9b76679a74d060ca7e
max-ang <max.angletti@gmail.com>
2016-02-19 15:39:31 -0300

    master con tags + prods + htaccess

--- html/master.php
+++ html/master.php
@@ -19,7 +19,7 @@ require_once (__DIR__) . '/../php/config.php';
             var PATH_CONTROLLER = '<?php echo PATH_CONTROLLER ?>';
             var PATH_HOME = '<?php echo PATH_HOME ?>';
         </script> 
-        
+
         <!-- anclas -->
         <script type="text/javascript">
             $(function () {
@@ -40,20 +40,21 @@ require_once (__DIR__) . '/../php/config.php';
 
         <!-- Botón UP -->
         <script>
-            $(window).scroll(function(){
-                    if ($(this).scrollTop() > 500) {
-                        $('.scrollUp').fadeIn();
-                    } else {
-                        $('.scrollUp').fadeOut();
-                    }
+            $(window).scroll(function () {
+                if ($(this).scrollTop() > 500) {
+                    $('.scrollUp').fadeIn();
+                } else {
+                    $('.scrollUp').fadeOut();
+                }
             });
-            $(document).ready(function(){
-                 $('.scrollUp').click(function(){
-                     $("html, body").animate({ scrollTop: 0 }, 600);
-                     return false;
-                 });
+            $(document).ready(function () {
+                $('.scrollUp').click(function () {
+                    $("html, body").animate({scrollTop: 0}, 600);
+                    return false;
+                });
             })
         </script>
+        <script type="text/javascript" src="http://nexus.ensighten.com/colgatepalmolive/Bootstrap.js"></script>
     </head>
     <body>
         <header>
@@ -67,22 +68,20 @@ require_once (__DIR__) . '/../php/config.php';
         </header>  
 
         <?php
-        
         require_once (__DIR__) . '/home.php';
         require_once (__DIR__) . '/registro.php';
         require_once (__DIR__) . '/productos.php';
         require_once (__DIR__) . '/bases.php';
-        
         ?>
 
         <!-- FOOTER -->
         <footer>
-            <p class="copyright"> © 2016 <a href="http://www.colgatepalmolive.com.ar" target="_blank">Colgate-Palmolive Company</a>. Todos los derechos reservados. Usted está viendo la página de Argentina. <br>Vea nuestras <a href="http://www.colgate.com.ar/app/Colgate/AR/Corp/LegalPrivacy.cvsp" target="_blank">Políticas de Privacidad</a>. </p> 
-            <p class="brand"> <a href="http://www.colgatepalmolive.com.ar" target="_blank"></a> </p>
+            <p class="copyright"> © 2016 <a onclick="javascript:sendLinkEvent('','www.volviendoalcole.com.ar/www.colgatepalmolive.com.ar');" href="http://www.colgatepalmolive.com.ar" target="_blank">Colgate-Palmolive Company</a>. Todos los derechos reservados. Usted está viendo la página de Argentina. <br>Vea nuestras <a onclick="javascript:sendLinkEvent('','www.volviendoalcole.com.ar/www.colgate.com.ar/app/Colgate/AR/Corp/LegalPrivacy.cvsp');" href="http://www.colgate.com.ar/app/Colgate/AR/Corp/LegalPrivacy.cvsp" target="_blank">Políticas de Privacidad</a>. </p> 
+            <p class="brand"> <a onclick="javascript:sendLinkEvent('','www.volviendoalcole.com.ar/www.colgatepalmolive.com.ar');" href="http://www.colgatepalmolive.com.ar" target="_blank"></a> </p>
         </footer>
 
         <div class="contentUp"><a href="#" class="scrollUp"></a></div>
-        
+
         <div id="divLoading">
             <img width="48" height="48" src="<?php echo PATH_IMAGES; ?>loading.gif">
             <p>Espere un momento.</p>
@@ -95,5 +94,38 @@ require_once (__DIR__) . '/../php/config.php';
             });
         </script>
         <script src="<?php echo PATH_JS ?>jqFuncs.js"></script>
+
+        <!-- Dynamic SiteCatalyst code version: H.x. Copyright 1997-2004 Omniture, Inc. More info available at http://www.omniture.com --> 
+        <script language="JavaScript" type="text/javascript">
+        <!--
+                    var s_account = 'CPARAll,CPARPromo';
+        -->
+        </script>
+        <script language='JavaScript' src='http://www.colgate.com/Colgate/Common/s_code_remote_h.js'></script>
+        <script language="JavaScript">
+        <!-- 
+                    _omniture.pageName = 'http://www.volviendoalcole.com.ar/Colgate';
+                    _omniture.server = 'http://www.volviendoalcole.com.ar';
+                    _omniture.channel = 'main';
+                    _omniture.account = 'CPARAll,CPARPromo';
+                    _omniture.prop1 = 'Latin America';
+                    _omniture.prop2 = 'Argentina';
+                    _omniture.prop3 = 'Spanish';
+                    _omniture.prop4 = 'Colgate Promo';
+                    _omniture.prop5 = 'Viví con Colgate la Mejor Vuelta al Cole';
+                    _omniture.linkInternalFilter = 'javascript:,/Colgate';
+                    _omniture.hier1 = 'Colgate Universal,';
+        -->
+        </script> 
+        <script language='JavaScript' src='http://www.colgate.com/Colgate/Common/s_code_remote_h_post.js'></script>
+        <script language="JavaScript">
+        <!--
+                    var s_code = _omniture.t();
+                    if (s_code) {
+                        alert("s_code = " + s_code);
+                                document.write(s_code);
+            } //--></script> 
+        <!-- End SiteCatalyst code version: H.x. --> 
+        <noscript><img src="http://CPUYAll.112.2o7.net/b/ss/CPARAll,CPARPromo/1/1/H.7--NS/123456?pageName=http://www.volviendoalcole.com.ar/Colgate" width="1" height="1" border="0" /></noscript>
     </body>
 </html>
